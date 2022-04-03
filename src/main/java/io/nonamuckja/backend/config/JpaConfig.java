d585efca6d0a017254908eef8d12435731705827
clroot <abcdkh1209@gmail.com>
2022-04-04 00:46:25 +0900

    enable JpaAuditing and implement BaseTimeEntity

---
+++ src/main/java/io/nonamuckja/backend/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package io.nonamuckja.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
