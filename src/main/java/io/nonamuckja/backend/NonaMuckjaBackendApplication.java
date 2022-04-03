b5f03a22b01127403df021f29b9a10c40cc4b8c6
clroot <abcdkh1209@gmail.com>
2022-04-03 19:39:20 +0900

    init project by spring initializer
    - use spring boot 2.6.6 with java 17

---
+++ src/main/java/io/nonamuckja/backend/NonaMuckjaBackendApplication.java
@@ -0,0 +1,12 @@
+package io.nonamuckja.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonaMuckjaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NonaMuckjaBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/io/nonamuckja/backend/NonaMuckjaBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package io.nonamuckja.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonaMuckjaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
