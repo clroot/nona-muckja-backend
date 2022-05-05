37bd1488e87bdf65c65f4af608235aa038bcb430
clroot <abcdkh1209@gmail.com>
2022-05-05 22:06:15 +0900

    refactor SecurityConfig

--- src/main/java/io/nonamuckja/backend/config/SecurityConfig.java
+++ src/main/java/io/nonamuckja/backend/config/SecurityConfig.java
@@ -3,12 +3,16 @@ package io.nonamuckja.backend.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -19,7 +23,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").hasRole("USER");
         http.formLogin();
         http.csrf().disable();
         http.logout();
--- src/main/java/io/nonamuckja/backend/controller/BaseController.java
+++ src/main/java/io/nonamuckja/backend/controller/BaseController.java
@@ -2,6 +2,7 @@ package io.nonamuckja.backend.controller;
 
 import io.nonamuckja.backend.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,5 +17,12 @@ public class BaseController {
     @ResponseBody
     public String index() {
         return "index";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/auth")
+    @ResponseBody
+    public String auth() {
+        return "auth";
     }
 }
