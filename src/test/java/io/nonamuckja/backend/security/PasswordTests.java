efac0532c88626c842987473ec9aa8bd68517ca3
clroot <abcdkh1209@gmail.com>
2022-05-05 15:51:48 +0900

    set Spring Security Config
    - with PasswordEncoder test

---
+++ src/test/java/io/nonamuckja/backend/security/PasswordTests.java
@@ -0,0 +1,34 @@
+package io.nonamuckja.backend.security;
+
+import io.bloco.faker.Faker;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class PasswordTests {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    @DisplayName("패스워드 인코더 테스트")
+    public void passwordEncoderTest() {
+        //given
+        Faker faker = new Faker();
+        String password = faker.internet.password();
+
+        //when
+        String encodedPassword = passwordEncoder.encode(password);
+        boolean isPasswordMatch = passwordEncoder.matches(password, encodedPassword);
+
+        //then
+        assertTrue(isPasswordMatch);
+        assertNotEquals(password, encodedPassword);
+    }
+}
