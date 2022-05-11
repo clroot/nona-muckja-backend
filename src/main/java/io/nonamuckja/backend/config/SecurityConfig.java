0197539791a12602a71686f6d6b4203393b1654d
clroot <abcdkh1209@gmail.com>
2022-05-12 01:21:37 +0900

    apply JwtRequestFilter to SecurityConfig

--- src/main/java/io/nonamuckja/backend/config/SecurityConfig.java
+++ src/main/java/io/nonamuckja/backend/config/SecurityConfig.java
@@ -2,26 +2,34 @@ package io.nonamuckja.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import io.nonamuckja.backend.security.jwt.JwtRequestFilter;
 import io.nonamuckja.backend.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 @Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	private final JwtRequestFilter jwtRequestFilter;
 	private final CustomOAuth2UserService customOAuth2UserService;
 
-	public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
-		this.customOAuth2UserService = customOAuth2UserService;
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 	@Bean
@@ -29,12 +37,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.formLogin().and();
 		http.csrf().disable();
-		http.logout();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 		http.oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
-
 	}
 }
