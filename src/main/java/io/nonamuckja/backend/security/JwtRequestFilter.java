e40e34f5bda5084e6447655aa3d1d51a89720a16
clroot <abcdkh1209@gmail.com>
2022-05-12 14:41:20 +0900

    refactor JwtRequestFilter

--- src/main/java/io/nonamuckja/backend/security/JwtRequestFilter.java
+++ src/main/java/io/nonamuckja/backend/security/JwtRequestFilter.java
@@ -24,38 +24,50 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class JwtRequestFilter extends OncePerRequestFilter {
+	private static final String AUTHORIZATION_HEADER = "Authorization";
+	private static final String REFRESH_HEADER = "Refresh";
+	private static final String BEARER_PREFIX = "Bearer ";
 	private final UserDetailsService userDetailsService;
 	private final JwtTokenUtils jwtTokenUtils;
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-		FilterChain filterChain) throws ServletException, IOException {
-		final String requestTokenHeader = request.getHeader("Authorization");
-		if (StringUtils.startsWith(requestTokenHeader, "Bearer ")) {
-			String jwtToken = requestTokenHeader.substring(7);
-			try {
-				String username = jwtTokenUtils.getUsernameFromToken(jwtToken);
-				if (StringUtils.isNotEmpty(username)) {
-					UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-					if (jwtTokenUtils.validateToken(jwtToken, userDetails)) {
-						UsernamePasswordAuthenticationToken authentication =
-							new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+		throws ServletException, IOException {
+		final String jwtToken = resolveToken(request);
 
-						authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-						SecurityContextHolder.getContext().setAuthentication(authentication);
-
-					}
-				}
-			} catch (IllegalStateException e) {
-				logger.error("Unable to fetch JWT Token");
-			} catch (ExpiredJwtException e) {
-				logger.error("JWT Token has expired");
-			} catch (Exception e) {
-				logger.error(e.getMessage());
-			}
-		} else {
+		if (jwtToken == null) {
 			logger.warn("JWT Token does not begin with Bearer String");
+			filterChain.doFilter(request, response);
+			return;
 		}
+
+		try {
+			String username = jwtTokenUtils.getUsernameFromToken(jwtToken);
+			UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+			if (jwtTokenUtils.validateToken(jwtToken, userDetails)) {
+				UsernamePasswordAuthenticationToken authentication =
+					new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+				authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+			}
+		} catch (IllegalStateException e) {
+			logger.error("Unable to fetch JWT Token");
+		} catch (ExpiredJwtException e) {
+			logger.error("JWT Token has expired");
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+
 		filterChain.doFilter(request, response);
+	}
+
+	private String resolveToken(HttpServletRequest request) {
+		String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+		if (StringUtils.isEmpty(bearerToken) || !bearerToken.startsWith(BEARER_PREFIX)) {
+			return null;
+		}
+		return bearerToken.substring(BEARER_PREFIX.length());
 	}
 }
