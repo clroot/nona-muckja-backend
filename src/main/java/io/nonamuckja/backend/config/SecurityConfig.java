b63fd094f1cceefa0d8928f530e6890bd6044b10
PARK9898 <45031880+PARK9898@users.noreply.github.com>
2022-05-13 16:46:31 +0900

    move OAuth secret to application-oauth-secret.yml

--- src/main/java/io/nonamuckja/backend/config/SecurityConfig.java
+++ src/main/java/io/nonamuckja/backend/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package io.nonamuckja.backend.config;
 
-import io.nonamuckja.backend.service.CustomOAuth2UserService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -11,29 +9,32 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import io.nonamuckja.backend.service.CustomOAuth2UserService;
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final CustomOAuth2UserService customOAuth2UserService;
+	private final CustomOAuth2UserService customOAuth2UserService;
 
-    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
-        this.customOAuth2UserService = customOAuth2UserService;
-    }
+	public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+		this.customOAuth2UserService = customOAuth2UserService;
+	}
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin().and();
-        http.csrf().disable();
-        http.logout();
-        http.oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.formLogin().and();
+		http.csrf().disable();
+		http.logout();
+		http.oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
 
-    }
+	}
 }
--- src/main/java/io/nonamuckja/backend/service/CustomOAuth2UserService.java
+++ src/main/java/io/nonamuckja/backend/service/CustomOAuth2UserService.java
@@ -13,10 +13,13 @@ import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
     private final UserRepository userRepository;
 
@@ -32,15 +35,25 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
         User user = saveOrUpdate(attributes);
 
-        return new DefaultOAuth2User(user.getRoles().stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role.name())).collect(Collectors.toList()),
-                attributes.getAttributes(),
-                attributes.getNameAttributeKey());
+        return new DefaultOAuth2User(user.getRoles().stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role.name())).collect(Collectors.toList()), attributes.getAttributes(), attributes.getNameAttributeKey());
     }
 
     private User saveOrUpdate(OAuthAttributes attributes) {
-        User user = userRepository.findByEmail(attributes.getEmail())
-                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
-                .orElse(attributes.toEntity());
-        return userRepository.save(user);
+        String email = attributes.getEmail();
+        String username = attributes.getName();
+        String picture = attributes.getPicture();
+
+        Optional<User> result = userRepository.findByEmail(email);
+        User user;
+
+        if (result.isPresent()) {
+            user = result.get();
+            user.update(username, picture);
+        } else {
+            user = User.builder().email(email).username(username).picture(picture).build();
+            userRepository.save(user);
+        }
+
+        return user;
     }
 }
