ab1e18a57a36db3b79fb33c8704bdbaf23e04acb
clroot <abcdkh1209@gmail.com>
2022-05-16 16:34:17 +0900

    refactor TestCode

--- src/test/java/io/nonamuckja/backend/TestUtils.java
+++ src/test/java/io/nonamuckja/backend/TestUtils.java
@@ -1,26 +1,50 @@
 package io.nonamuckja.backend;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import io.bloco.faker.Faker;
 import io.nonamuckja.backend.domain.Address;
+import io.nonamuckja.backend.domain.party.Party;
+import io.nonamuckja.backend.domain.party.PartyRepository;
 import io.nonamuckja.backend.domain.user.User;
 import io.nonamuckja.backend.domain.user.UserRepository;
-import io.nonamuckja.backend.domain.user.UserRole;
+import io.nonamuckja.backend.service.AuthService;
+import io.nonamuckja.backend.service.PartyService;
 import io.nonamuckja.backend.web.dto.AddressDTO;
+import io.nonamuckja.backend.web.dto.PartyRegisterFormDTO;
+import io.nonamuckja.backend.web.dto.UserDTO;
 import io.nonamuckja.backend.web.dto.UserRegisterFormDTO;
 
 @Component
 @Transactional
 public class TestUtils {
-	@Autowired
-	private PasswordEncoder passwordEncoder;
 
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private PartyRepository partyRepository;
+
+	@Autowired
+	private PartyService partyService;
+
+	@Autowired
+	private AuthService authService;
+
+	public User createUser(String email, String username, String password) {
+		UserRegisterFormDTO userRegisterDTO = UserRegisterFormDTO.builder()
+			.email(email)
+			.username(username)
+			.password(password)
+			.address(createAddressDTO())
+			.build();
+
+		Long registeredId = authService.register(userRegisterDTO);
+
+		return userRepository.findById(registeredId).orElseThrow();
+	}
 
 	public User createUser() {
 		Faker faker = new Faker();
@@ -32,34 +56,20 @@ public class TestUtils {
 		return createUser(email, username, password);
 	}
 
-	public User createUser(String email, String username, String password) {
-		User user = User.builder()
-			.email(email)
-			.username(username)
-			.password(passwordEncoder.encode(password))
-			.address(createAddress())
+	public Party createParty(User host, Long limitMemberCount, AddressDTO address) {
+		PartyRegisterFormDTO partyRegisterDTO = PartyRegisterFormDTO.builder()
+			.address(address)
+			.limitMemberCount(limitMemberCount)
 			.build();
+		UserDTO hostDTO = UserDTO.fromEntity(host);
 
-		user.addRole(UserRole.USER);
-		userRepository.save(user);
+		Long createdPartyId = partyService.createParty(partyRegisterDTO, hostDTO);
 
-		return user;
+		return partyRepository.findById(createdPartyId).orElseThrow();
 	}
 
-	public void deleteUser(User user) {
-		userRepository.delete(user);
-	}
-
-	public Address createAddress() {
-		Faker faker = new Faker();
-
-		return Address.builder()
-			.address(faker.address.city() + " " + faker.address.secondaryAddress())
-			.roadAddress(faker.address.streetAddress())
-			.zipCode(faker.address.postcode())
-			.x(Double.parseDouble(faker.number.decimal(2, 14)))
-			.y(Double.parseDouble(faker.number.decimal(3, 14)))
-			.build();
+	public Party createParty(User host, Long limitMemberCount) {
+		return createParty(host, limitMemberCount, createAddressDTO());
 	}
 
 	public UserRegisterFormDTO createUserRegisterFormDTO() {
@@ -86,5 +96,9 @@ public class TestUtils {
 			.x(Double.parseDouble(faker.number.decimal(2, 14)))
 			.y(Double.parseDouble(faker.number.decimal(3, 14)))
 			.build();
+	}
+
+	public Address createAddress() {
+		return createAddressDTO().toEntity();
 	}
 }
