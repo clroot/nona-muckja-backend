8b5e73fa25120575760663eff99709944e906491
clroot <abcdkh1209@gmail.com>
2022-05-27 00:43:34 +0900

    implement CustomPartyRepository by queryDsl & PartySearch

--- src/main/java/io/nonamuckja/backend/domain/party/PartyRepository.java
+++ src/main/java/io/nonamuckja/backend/domain/party/PartyRepository.java
@@ -1,9 +1,11 @@
 package io.nonamuckja.backend.domain.party;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PartyRepository extends JpaRepository<Party, Long> {
+public interface PartyRepository
+	extends JpaRepository<Party, Long>, JpaSpecificationExecutor<Party>, CustomPartyRepository {
 
 }
