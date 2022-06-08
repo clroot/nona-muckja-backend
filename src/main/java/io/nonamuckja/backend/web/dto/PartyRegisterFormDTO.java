abac4d6f68551db4e7cf92f0dd31f8f1eca1849b
clroot <abcdkh1209@gmail.com>
2022-06-08 13:24:48 +0900

    PartyRegisterFormDTO @ToString

--- src/main/java/io/nonamuckja/backend/web/dto/PartyRegisterFormDTO.java
+++ src/main/java/io/nonamuckja/backend/web/dto/PartyRegisterFormDTO.java
@@ -15,12 +15,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class PartyRegisterFormDTO {
 	@NotBlank
 	private String title;
